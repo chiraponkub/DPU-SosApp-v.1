ed4acfafcc52d237a8a27e4f5ede460d799651e6
Chirapon Hemtrakan <Chiraponkub@hotmail.com>
2022-12-09 15:19:50 +0700

    update log

--- restapi/registersAPI.go
+++ restapi/registersAPI.go
@@ -3,11 +3,13 @@ package restapi
 import (
 	"github.com/chiraponkub/DPU-SosApp-v.1.git/constant"
 	singup "github.com/chiraponkub/DPU-SosApp-v.1.git/restapi/model/singup/request"
+	"github.com/chiraponkub/DPU-SosApp-v.1.git/utility/logs"
 	"github.com/chiraponkub/DPU-SosApp-v.1.git/utility/response"
 	"github.com/labstack/echo/v4"
 )
 
 func (ctrl Controller) SendOTP(c echo.Context) error {
+	logs.LogStart("SendOTP")
 	var request = new(singup.PhoneNumber)
 	var res response.RespMag
 	err := c.Bind(request)
@@ -19,18 +21,23 @@ func (ctrl Controller) SendOTP(c echo.Context) error {
 	if err != nil {
 		res.Code = constant.ErrorCode
 		res.Msg = err.Error()
+		logs.LogError(err)
 		return response.EchoError(c, 400, res)
 	}
+	logs.LogRequest(request)
+
 	resp, err := ctrl.SentOTPLogic(request)
 	if err != nil {
 		res.Code = constant.ErrorCode
 		res.Msg = err.Error()
+		logs.LogError(err)
 		return response.EchoError(c, 400, res)
 	}
 
 	res.Msg = constant.SuccessMsg
 	res.Code = constant.SuccessCode
 	res.Data = resp
+	logs.LogResponse(res)
 	return response.EchoSucceed(c, res)
 }
 
---
+++ utility/logs/log.go
@@ -0,0 +1,38 @@
+package logs
+
+import (
+	"log"
+	"time"
+)
+
+//var times = time.Now().Add(time.Minute * 3).Add(time.Hour * 7)
+
+func LogStart(nameAPI string) {
+	log.Println("---------------------- Start ---------------------")
+	log.Printf("Start API : %s , Time : %v", nameAPI, time.Now().Add(time.Minute*3).Add(time.Hour*7))
+}
+
+func LogRequest(req interface{}) {
+	log.Println("")
+	log.Println("--------- Start Log Request -----------")
+	log.Printf("Request : %v", req)
+	log.Println(" -------- End Log Request -------------")
+}
+
+func LogResponse(res interface{}) {
+	log.Println("")
+	log.Println("--------- Start Log Response ---------")
+	log.Printf("Response : %v", res)
+	log.Printf("Time : %v", time.Now().Add(time.Minute*3).Add(time.Hour*7))
+	log.Println("--------- End Log Response -----------")
+	log.Println("------------------------ End ----------------------")
+}
+
+func LogError(err error) {
+	log.Println("")
+	log.Println(" -------- Start Log Error ---------")
+	log.Printf("Error : %v", err.Error())
+	log.Printf("Time : %v", time.Now().Add(time.Minute*3).Add(time.Hour*7))
+	log.Println("--------- End Log Error -----------")
+	log.Println("------------------------ End ----------------------")
+}

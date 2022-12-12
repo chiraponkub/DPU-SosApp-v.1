980e1557146ca21263040e5afcb1251623ff5f6c
Chirapon Hemtrakan <Chiraponkub@hotmail.com>
2022-12-12 09:49:26 +0700

    update log

--- restapi/loginAPI.go
+++ restapi/loginAPI.go
@@ -4,21 +4,25 @@ import (
 	"github.com/chiraponkub/DPU-SosApp-v.1.git/constant"
 	singin "github.com/chiraponkub/DPU-SosApp-v.1.git/restapi/model/singin/request"
 	singinResp "github.com/chiraponkub/DPU-SosApp-v.1.git/restapi/model/singin/response"
+	"github.com/chiraponkub/DPU-SosApp-v.1.git/utility/logs"
 	"github.com/chiraponkub/DPU-SosApp-v.1.git/utility/response"
 	"github.com/labstack/echo/v4"
 )
 
 func (ctrl Controller) SignInUser(c echo.Context) error {
+	logs.LogStart("SignInUser")
 	var request = new(singin.Login)
 	var res response.RespMag
 	err := c.Bind(request)
 	if err != nil {
 		return response.EchoError(c, 400, err)
 	}
+	logs.LogRequest(request)
 	token, err := ctrl.LoginLogic(request)
 	if err != nil {
 		res.Msg = err.Error()
 		res.Code = constant.ErrorCode
+		logs.LogError(err)
 		return response.EchoSucceed(c, res)
 	}
 
@@ -28,6 +32,7 @@ func (ctrl Controller) SignInUser(c echo.Context) error {
 	res.Msg = constant.SuccessMsg
 	res.Code = constant.SuccessCode
 	res.Data = resp
+	logs.LogResponse(res)
 	return response.EchoSucceed(c, resp)
 }
 
--- utility/logs/log.go
+++ utility/logs/log.go
@@ -9,7 +9,7 @@ import (
 
 func LogStart(nameAPI string) {
 	log.Println("---------------------- Start ---------------------")
-	log.Printf("Start API : %s , Time : %v", nameAPI, time.Now().Add(time.Minute*3).Add(time.Hour*7))
+	log.Printf("Start API : %s , Time : %v", nameAPI, time.Now().Add(time.Hour*7))
 }
 
 func LogRequest(req interface{}) {
@@ -23,7 +23,7 @@ func LogResponse(res interface{}) {
 	log.Println("")
 	log.Println("--------- Start Log Response ---------")
 	log.Printf("Response : %v", res)
-	log.Printf("Time : %v", time.Now().Add(time.Minute*3).Add(time.Hour*7))
+	log.Printf("Time : %v", time.Now().Add(time.Hour*7))
 	log.Println("--------- End Log Response -----------")
 	log.Println("------------------------ End ----------------------")
 }
@@ -32,7 +32,7 @@ func LogError(err error) {
 	log.Println("")
 	log.Println(" -------- Start Log Error ---------")
 	log.Printf("Error : %v", err.Error())
-	log.Printf("Time : %v", time.Now().Add(time.Minute*3).Add(time.Hour*7))
+	log.Printf("Time : %v", time.Now().Add(time.Hour*7))
 	log.Println("--------- End Log Error -----------")
 	log.Println("------------------------ End ----------------------")
 }
